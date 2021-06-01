b1529ae83a41e8b4554bd87d06c630220aaf4471
Itamar Junior <itjunior@gmail.com>
2021-06-01 14:28:13 +0100

    First commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+##Revision of Array
+###Write statements to the following:
+(a) an array of floating-point numbers with ten elements
+
+(b) an array of characters with eight elements
+
+(c) an array of integers with 11 elements
+
+(d) an array of integers initialised to 123,156,77,-9,78,21
+
+(e) assign values to a 5-element array of integers with values entered from the keyboard.
+
+(f) assign values to a 6-element array of Strings with strings entered from the keyboard.
+
+Solution:WritingArray
--- src/WritingArray.java
+++ src/WritingArray.java
@@ -13,12 +13,22 @@ public class WritingArray {
         int[] integerArrayInitialised = {123,156,77,-9,78,21};
         //(e) assign values to a 5 - element array of integers with values entered from the keyboard.
         Scanner keyboard = new Scanner(System.in);
-        int[] assignArrayKeyboard = {keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt()};
+        int[] assignArrayKeyboard = new int[5];
+        for(int count = 0; count < assignArrayKeyboard.length ; count++){
+            System.out.println("Input the value #"+(count+1));
+            assignArrayKeyboard[count] = keyboard.nextInt();
+        }
+
         for (int count = 0; count < assignArrayKeyboard.length ; count++){
         System.out.println(assignArrayKeyboard[count]);
         }
         //(f) assign values to a 6 - element array of Strings with strings entered from the keyboard.
-        String[] stringArray = {keyboard.next(),keyboard.next(),keyboard.next()};
+        String[] stringArray = new String[6];
+        for(int count = 0; count < stringArray.length ; count++){
+            System.out.println("Input the String #"+(count+1));
+            stringArray[count]= keyboard.next();
+        }
+
         for (int count = 0; count < stringArray.length ; count ++){
             System.out.println(stringArray[count]);
         }
